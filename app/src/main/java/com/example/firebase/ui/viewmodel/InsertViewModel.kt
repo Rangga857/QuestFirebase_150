b905e7728a51bb5a94a93324e017b23c24f37952
Rangga857 <azharrangga28@gmail.com>
2025-01-10 20:31:52 +0700

    Create data class FormErrorState untuk validasi form mahasiswa

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,19 @@ package com.example.firebase.ui.viewmodel
 
 import com.example.firebase.model.Mahasiswa
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+){
+    fun isValid(): Boolean{
+        return nim == null && nama == null && jenisKelamin == null
+                && alamat == null && kelas == null && angkatan == null
+    }
+}
 fun MahasiswaEvent.toMhsModel():Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,

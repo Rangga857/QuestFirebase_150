d5ed3eb0f8d4ef63f94bb36ebcbca027555a17de
Rangga857 <azharrangga28@gmail.com>
2025-01-10 20:20:10 +0700

    Create Data class MahasiswaEvent

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.firebase.ui.viewmodel
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)

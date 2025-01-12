dbe49e9c01967643ffc2e5820ea0b7c2d061037d
Rangga857 <azharrangga28@gmail.com>
2025-01-12 19:05:38 +0700

    Update code NetworkRepository deleteMhs

--- app/src/main/java/com/example/firebase/repository/NetworkRepository.kt
+++ app/src/main/java/com/example/firebase/repository/NetworkRepository.kt
@@ -23,11 +23,15 @@ class NetworkRepository (
 
     override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
         try {
-            firestore.collection("Mahasiswa")
-                .document(mahasiswa.nim)
-                .delete()
+            val querySnapshot = firestore.collection("Mahasiswa")
+                .whereEqualTo("nim", mahasiswa.nim)
+                .get()
                 .await()
-        }catch (e: Exception) {
+
+            for (document in querySnapshot.documents) {
+                document.reference.delete().await()
+            }
+        } catch (e: Exception) {
             throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
         }
     }
